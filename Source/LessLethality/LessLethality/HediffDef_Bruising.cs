8c4fd0e8e5f370c4cf0956175ee6df7c87a688c3
NukeNFighted <zauer@live.se>
2017-07-14 14:47:57 +0200

    Added classes for the Bruising Hediff

---
+++ Source/LessLethality/LessLethality/HediffDef_Bruising.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace LessLethality
+{
+    public static class HediffDefOf_Bruising
+    {
+        public static HediffDef NNF_Bruising;
+    }
+}
---
+++ Source/LessLethality/LessLethality/ThingDef_BeanBag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace LessLethality
+{
+    public class ThingDef_BeanBag : ThingDef
+    {
+        public float AddHediffChance = 1f;
+        public HediffDef HediffToAdd = HediffDefOf_Bruising.NNF_Bruising;
+    }
+}
